cfb1ca3a5124ac2b945eda3f2a6620792cd8583c
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-02-25 15:28:47 +0100

    i did the first exercises

--- CS50_Introduction_to_Computer_Science/week_2_arrays/alphabe.c
+++ CS50_Introduction_to_Computer_Science/week_2_arrays/alphabe.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 /*
 *Check if a lowercase string's characters are in alphabetical order.
 *if yes , print "YES".
 *if no print "No".
 */
+int main(void)
+{
+    string word = get_string("word: ");
+
+    int word_lenght = strlen(word);
+    for (int i = 0; i < word_lenght - 1; i++)
+    {
+        //check if not alphabetical (i.e., "ba")
+        if (word[i] > word[i + 1])
+        {
+            printf("No\n");
+            return 0;
+        }
+    }
+
+    printf("Yes\n");
+    return 0;
+}
